06d24dd7645ac2ef78857c3a50a87e6a27b533de
Francesco Scollo <Francesco Scollo>
2017-02-01 00:11:22 +0000

    Add project boilerplate

---
+++ list_button.js
@@ -0,0 +1,4 @@
+$(".list1").hide();
+$("#l1").click(function(){
+	$(".list1").next().slideToggle("slow");
+});
---
+++ table_button.js
@@ -0,0 +1,4 @@
+$(".tab1").hide();
+$("#button1").click(function() {
+	$(".tab1").slideToggle("slow");
+});
